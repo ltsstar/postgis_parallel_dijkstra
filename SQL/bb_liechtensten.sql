242515ea6fe83acdec5073b9f3c31633dbba8312
Michel Kunkler <michel.kunkler@gmail.com>
2018-11-07 10:01:47 +0100

    added bounding box liechtenstein

---
+++ SQL/bb_liechtensten.sql
@@ -0,0 +1,4 @@
+insert into dijkstra_lines VALUES (0, 0, 1, st_geomfromtext('LINESTRING(9 45.5, 9 48.2)', 4326));
+insert into dijkstra_lines VALUES (0, 0, 1, st_geomfromtext('LINESTRING(10 45.5, 10 48.2)', 4326));
+insert into dijkstra_lines VALUES (0, 0, 1, st_geomfromtext('LINESTRING(9 45.5, 10 45.5)', 4326));
+insert into dijkstra_lines VALUES (0, 0, 1, st_geomfromtext('LINESTRING(9 48.2, 10 48.2)', 4326));
--- SQL/create_voronoi_table.sql
+++ SQL/create_voronoi_table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS voronoi_cells;
 CREATE TABLE voronoi_cells (
 	osm_id BIGINT NOT NULL,
 	sub_id INT NOT NULL,
-	class INT,
-	PRIMARY KEY(osm_id, sub_id)
+	class INT
+	/* PRIMARY KEY(osm_id, sub_id) */
 );
-SELECT AddGeometryColumn ('voronoi_cells','poly',4326,'POLYGON',2);
+SELECT AddGeometryColumn ('voronoi_cells','poly',4326,'LINESTRING',2);
